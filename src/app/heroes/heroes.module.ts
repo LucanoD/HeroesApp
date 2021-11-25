24da598b19ff88733f9a75509cac2f48f1865f67
Dani Lucano <Olucano10@gmail.com>
2021-11-25 02:27:37 -0500

    Menu de Heroes

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule  }  from  '@angular/flex-layout' ;
+
 import { AgregarComponent } from './page/agregar/agregar.component';
 import { BuscarComponent } from './page/buscar/buscar.component';
 import { HeroeComponent } from './page/heroe/heroe.component';
 import { HomeComponent } from './page/home/home.component';
 import { ListadoComponent } from './page/listado/listado.component';
 import { HeroesRoutingModule } from './heroes-routing.module';
+import { MaterialModule } from '../material/material.module';
+import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
+import { ImagenPipe } from './pipes/imagen.pipe';
 
 
 
@@ -15,10 +20,14 @@ import { HeroesRoutingModule } from './heroes-routing.module';
     BuscarComponent,
     HeroeComponent,
     HomeComponent,
-    ListadoComponent
+    ListadoComponent,
+    HeroeTarjetaComponent,
+    ImagenPipe
   ],
   imports: [
     CommonModule,
+    FlexLayoutModule,
+    MaterialModule,
     HeroesRoutingModule
   ]
 })
