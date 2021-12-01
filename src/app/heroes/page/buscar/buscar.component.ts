d0bbbbb974158c2896343cc12702d4ae7f5363b3
Dani Lucano <Olucano10@gmail.com>
2021-12-01 18:09:26 -0500

    Terminado

--- src/app/heroes/page/buscar/buscar.component.ts
+++ src/app/heroes/page/buscar/buscar.component.ts
@@ -1,5 +1,7 @@
+import { MatAutocompleteSelecdEvent } from '@angular/material/autocomplete';
 import { Component, OnInit } from '@angular/core';
-
+import { Heroe } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
 @Component({
   selector: 'app-buscar',
   templateUrl: './buscar.component.html',
@@ -8,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuscarComponent implements OnInit {
 
-  constructor() { }
+  termino: string = '';
+  heroes: Heroe[] = [];
+  heroeSeleccionado!: Heroe;
+
+  constructor(private heroesService: HeroesService ) { }
 
   ngOnInit(): void {
+  }
+
+  buscando(){
+
+    this.heroesService.getSugerencias( this.termino)
+      .subscribe( heroes => this.heroes = heroes );
+  }
+
+  opcionSeleccionada( event: MatAutocompleteSelecdEvent) {
+    const heroe: Heroe = event.option.value;
+    this.termino =heroe.superhero;
+
+    this.heroesService.getHeroePorId ( heroe.id!)
+      .subscribe( heroe => this.heroeSeleccionado = heroe );
   }
 
 }
